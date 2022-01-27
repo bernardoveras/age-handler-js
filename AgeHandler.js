8c4f0f29691f0600ecdc404350b19f194fc8cc3b
Bernardo Veras <bernardo@vvssistemas.com.br>
2022-01-27 18:37:18 -0300

    Add isYoungerThan function

--- AgeHandler.js
+++ AgeHandler.js
@@ -22,14 +22,17 @@ class AgeHandler {
   }
 
   isOlderThan(age) { return this.ages.filter((v) => v > age); }
+  isYoungerThan(age) { return this.ages.filter((v) => v < age); }
 }
 
 const ageHandler = new AgeHandler([10, 12, 20, 22, 23, 24, 30, 40, 70]);
 
 const olders = ageHandler.olders();
 const youngs = ageHandler.youngs();
-const agesOlderThanTwenty = ageHandler.isOlderThan(21);
+const agesOlderThanTwenty = ageHandler.isOlderThan(20);
+const agesYoungerThanTwenty = ageHandler.isYoungerThan(20);
 
 console.log(`Olders: ${olders}`);
 console.log(`Youngs: ${youngs.length > 0 ? youngs : "None"}`);
 console.log(`Is older than 20: ${agesOlderThanTwenty.length > 0 ? agesOlderThanTwenty : "None"}`);
+console.log(`Is younger than 20: ${agesYoungerThanTwenty.length > 0 ? agesYoungerThanTwenty : "None"}`);
